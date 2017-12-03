30463316c7cfdb6aa11a478473e2cc8451662a7b
iwang2 <iwang2@stuy.edu>
2017-12-02 22:34:30 -0500

    got redirection to work from execute

--- shell.c
+++ shell.c
@@ -27,40 +27,7 @@ Specific Requirements:
 4. makefile!!
 */
 
-char ** parse_args(char * s){
-  char * line = (char *)calloc(256, sizeof(char));
-  strcpy(line, s);
-  printf("%s\n", line);
-  char ** args = (char **)calloc(6, sizeof(char *));
-  char * s1 = line;
-  int i;
-  for (i = 0; s1; i ++) {
-    args[i] = strsep(&s1, " ");
-  }
-  return args;
-}
-
-void execute(char * s){
-  char * line = (char *)calloc(256, sizeof(char));
-  strcpy(line, s);
-  //printf("%s\n", line);
-  char ** commands = (char **)calloc(6, sizeof(char *));
-  char * s1 = line;
-  int i;
-  for(i = 0; s1; i++){
-    commands[i] = clean(strsep(&s1, ";"));
-    //printf("commands[%d]: \"%s\"\n", i, commands[i]);
-  }
-
-  for(i = 0; commands[i]; i++){
-    execute_args(commands[i]);
-  }
-
-  free(line);
-  free(commands);
-}
-
-char * clean (char * s){
+char * clean (char * s) {
   //trim head whitespace
   int i = 0;
   while(s[i] == ' '){
@@ -78,14 +45,51 @@ char * clean (char * s){
   return s;
 }
 
-void execute_args(char * s){
+char ** parse_args (char * s) {
+  char * line = (char *)calloc(256, sizeof(char));
+  strcpy(line, s);
+  printf("%s\n", line);
+  char ** args = (char **)calloc(6, sizeof(char *));
+  char * s1 = line;
+  int i;
+  for (i = 0; s1; i ++) {
+    args[i] = strsep(&s1, " ");
+  }
+  return args;
+}
+
+void execute (char * s) {
+  char * line = (char *)calloc(256, sizeof(char));
+  strcpy(line, s);
+  //printf("%s\n", line);
+  char ** commands = (char **)calloc(6, sizeof(char *));
+  char * s1 = line;
+  int i;
+  for (i = 0; s1; i++) {
+    commands[i] = clean(strsep(&s1, ";"));
+    //printf("commands[%d]: \"%s\"\n", i, commands[i]);
+  }
+
+  for (i = 0; commands[i]; i++) {
+    if(!redirect(commands[i])){
+      execute_args(commands[i]);
+    }
+  }
+
+  free(line);
+  free(commands);
+}
+
+void execute_args (char * s) {
   char ** args = parse_args(s);
   int f = fork();
+  
   if(!f){
     execvp(args[0], args);
     free(args);
     exit(0);
   }
+  
   else{
     int status;
     wait(&status);
@@ -110,13 +114,13 @@ void execute_args(char * s){
   }
 }
 
-void cd(char * s){
+void cd (char * s) {
   chdir(s);
   char dir[256];
   printf("current directory: %s\n", getcwd(dir, sizeof(dir)));
 }
 
-void greater(char * command, char * file){
+void greater (char * command, char * file) {
   printf("Creating %s\n", file);
   int a = dup(1);
   int fd = open(file, O_CREAT | O_WRONLY, 0644);
@@ -124,4 +128,28 @@ void greater(char * command, char * file){
   execute(command);
   dup2(a, 1);
   close(fd);
+}
+
+int redirect (char * s) {
+  int i;
+  // put possible file name into separate string
+  char ** args = parse_args(s);
+  for(i = 0; args[i]; i++){}
+  char * file = (char *)malloc(sizeof(args[i]));
+  file = args[i-1];
+  
+  for (i = 0; s[i]; i++) {
+    if (s[i] == '>') {
+      *(s+i) = 0;
+      greater(s, file);
+      return 1;
+    }
+    /*
+    if (strcmp(s[i], "<") == 0) {
+    }
+    if (strcmp(s[i], "|") == 0) {
+    }
+    */
+  }
+  return 0;
 }
--- shell.h
+++ shell.h
@@ -1,13 +1,15 @@
 // insert headers here!!!
 
-char ** parse_args(char * s);
+char ** parse_args (char * s);
 
-void execute_args(char * s);
+void execute_args (char * s);
 
-void execute(char * s);
+void execute (char * s);
 
 char * clean (char * s);
 
-void cd(char * s);
+void cd (char * s);
 
-void greater(char * command, char * file);
+void greater (char * command, char * file);
+
+int redirect (char * s);
